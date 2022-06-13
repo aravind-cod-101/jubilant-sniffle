169df70f905c295801b85cfa0af9c5dc9b847f62
Aravind Balaji <61358471+aravind-cod-101@users.noreply.github.com>
2022-06-13 19:38:47 +0530

    Backend Api completed

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
+// import
 require("dotenv").config();
 const express = require("express");
-const PORT = process.env.PORT || 5000;
+const db = require("./db/index");
+
+// code
 const app = express();
 var restaurants = require("./restaurants.json");
 app.use(express.json());
@@ -10,58 +13,127 @@ app.get("/", (req, res) => {
 });
 
 // Get all restaurants
-app.get("/api/v1/restaurants", (req, res) => {
-  console.log("Getting all the restaurants...");
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurants,
-    },
-  });
+app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query("select * from restaurants");
+    console.log("Getting all the restaurants...");
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: {
+        restaurants: results.rows,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      status: "failed",
+      data: {
+        err,
+      },
+    });
+  }
 });
 
 // Get a single restaurant
-app.get("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req);
-  const id = req.params.id;
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    console.log(req);
 
-  res.send(id);
-  // res.status(200).json({
-  //     status: "success",
-  //     data:{
-
-  //     }
-  // })
+    const record = await db.query("select * from restaurants where id= $1", [
+      req.params.id,
+    ]);
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: record.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({
+      status: "failed",
+      data: {
+        err,
+      },
+    });
+  }
 });
 
 // Post method
-app.post("/api/v1/restaurants", (req, res) => {
-  console.log(req.body);
-  const data = req.body;
-  restaurants.push(data);
-  res.json({
-    status: "success",
-    data: {
-      restaurants,
-    },
-  });
+app.post("/api/v1/restaurants", async (req, res) => {
+  try {
+    console.log(req.body);
+    const data = req.body;
+    const results = await db.query(
+      "insert into restaurants (name,location,price) values($1,$2,$3) returning *",
+      [data.name, data.location, data.price]
+    );
+
+    res.json({
+      status: "success",
+      data: {
+        restaurant: results.rows,
+      },
+    });
+  } catch (err) {
+    res.json({
+      status: "failed",
+      data: {
+        err,
+      },
+    });
+  }
 });
 
 // update method
-app.put("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.body);
-  console.log(req.params.id);
-  res.send("Updated");
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    console.log(req.body);
+    console.log(req.params.id);
+    const result = await db.query(
+      "update restaurants set name = $1, location = $2, price=$3 where id = $4 returning *",
+      [req.body.name, req.body.location, req.body.price, req.params.id]
+    );
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: result.rows,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "failed",
+      data: {
+        err,
+      },
+    });
+  }
 });
 
 // delete method
 
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.body);
-  console.log(req.params.id);
-  res.send("Deleted");
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const results = await db.query("delete from restaurants where id = $1", [
+      req.params.id,
+    ]);
+    res.status(200).json({
+      status: "success",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({
+      status: "failed",
+      data: {
+        err,
+      },
+    });
+  }
 });
 // end of program
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is up and running at port ${PORT}`);
 });
